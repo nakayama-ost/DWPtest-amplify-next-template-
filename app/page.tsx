5c640df40d870260232adf61747030b5ecb56d3c
mit-nakayama <mit-nakayama@ostechnology.co.jp>
2024-12-17 14:05:44 +0900

    Add storage backedend

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { StorageBrowser } from "../components/StorageBrowser";
 
 Amplify.configure(outputs);
 
@@ -32,21 +34,18 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+            <h1>Hello {user?. username}</h1>
+            <button onClick={signOut}>Sign out</button>
+      
+          {/* StorageBrowser Component */}
+          <h2>Your Files</h2>
+          <StorageBrowser />
+      
+        </main>
+      )}
+    </Authenticator>
+  );  
 }
